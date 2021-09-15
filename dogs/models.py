4f8eab0b2d3583d0438cf6d9de71a2c217b00283
yeongroksong <yeongroksong@yeongroksongui-MacBookPro.local>
2021-09-15 21:00:05 +0900

    Add: owners, dogs database

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import aggregates
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Owners(models.Model):
+    name    = models.CharField(max_length=45)
+    email   = models.CharField(max_length=300)
+    age     = models.IntegerField()
+
+    class Meta:
+        db_table = "owners"
+
+class Dogs(models.Model):
+    owner_id = models.ForeignKey("Owners",on_delete=models.CASCADE)
+    name     = models.CharField(max_length=45)
+    age      = models.IntegerField()
+
+    class Meta:
+        db_table = "dogs"
+
+
